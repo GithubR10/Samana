d3f474be86d40ba3e3e3b4ed9b90096c95c7d674
GithubR10 <123190082+GithubR10@users.noreply.github.com>
2023-01-27 23:29:32 +0100

    Add files via upload

---
+++ es.ts
@@ -0,0 +1,57 @@
+const es = {
+  home: 'Home',
+  profile: 'Mi Perfil',
+  logout: 'Logout',
+  marketplace: 'Marketplace',
+  disconnect: 'Desconectar',
+  sold: 'INVERTIDO',
+  total: 'TOTAL',
+  youReceive: 'RECIBIRA:',
+  invest: 'Invertir',
+  rentPerNight: 'RENTA POR NOCHE',
+  upcomingListings: 'Próximos listados',
+  getNotified1: 'Sé notificado cuando esta propiedad se liste',
+  registerInterest: 'Registrar Interés',
+  alwaysBeFirst: 'Sé siempre el primero en la fila',
+  getNotified2: 'Recibe una notificación cuando se liste una nueva propiedad',
+  getNotifiedButton: 'Recibir notificaciones',
+  comingSoon: 'Próximamente',
+  comeBackToSeeNewListingsSoon: 'Vuelva pronto para ver nuevos anuncios',
+
+  // ID
+  notFound: 'No existe',
+  backHome: 'Regresar al inicio',
+  details: 'Detalles',
+  howMuchDoYouWantToInvest: '¿Cuánto quieres invertir?',
+  maxSupply: 'Máximo suministro',
+  approveContract: 'Aprobar el contrato',
+  youNeedASamanaCitizenNFTToParticipate:
+    'Necesitas Samaná Citizens NFT para participar',
+  currentlyNotForSale: 'Actualmente no esta en venta',
+  investmentProperty: 'Inversión Propiedad',
+
+  // METADATA
+  location: 'Ubicación',
+  roiPerYear: 'ROI por año',
+  roiFromSale: 'ROI de la venta',
+  estimatedNetRoi: 'ROI Neto estimado en 5 años',
+  estimatedRent: 'Alquiler estimado por noche',
+  constructionTime: 'Tiempo de construcción',
+  constructionCost: 'Coste de la construcción',
+  valuationCompletion: 'Valuación de la construcción al finalizar',
+  valuationFiveYears: 'Valuación de la construcción en 5 años',
+  stipulatedRentalPeriod: 'Periodo estipulado de alquiler antes de la venta',
+
+  // PROFILE
+  portfolioSize: 'Tamaño del portafolio',
+  landInvestments: 'Inversión en terreno',
+  propertyInvestments: 'Inversión en propiedades',
+  owned: 'Adquirido',
+  youAreNotAnNFTOwner: 'No eres propietario de ningún NFT',
+  yourPortfolio: 'Mi portafolio',
+  myInvestment: 'Mi inversión',
+  myLand: 'Mi terreno',
+  buyLand: 'Comprar terreno',
+};
+
+export default es;
